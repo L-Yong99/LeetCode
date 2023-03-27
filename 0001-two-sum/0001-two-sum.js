f218251a9b0574fd04e529a9df3c82f710951bf6
Leon Boon <107501061+L-Yong99@users.noreply.github.com>
2023-03-27 19:17:26 +0800

    Time: 182 ms (14.36%), Space: 42.2 MB (74.64%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -6,14 +6,8 @@
  
 var twoSum = function(nums, target) {
 
-  let difference = 0;
-  let result = [];
-
   // iterate through each item in array
   for (let i = 0; i < nums.length; i++) {
-
-      // guard clause for when result is found
-      if (result.length != 0) break;
 
       // deduct num from target, to find the difference
       difference = target - nums[i]
@@ -21,16 +15,41 @@ var twoSum = function(nums, target) {
       // check if leftover value exist in array
       if (
           nums.includes(difference) &&
-          nums.indexOf(difference) != i &&
-          target - nums[i] - difference === 0
+          nums.indexOf(difference) != i
           ) {
 
           // take the position of the two number into new array
-          result.push(i);
-          result.push(nums.indexOf(difference));
+          return [i, nums.indexOf(difference)]
       }
   };
-
-  return result;
   
 };
+
+// var twoSum = function(nums, target) {
+
+//   let difference = 0;
+//   let result = [];
+
+//   // iterate through each item in array
+//   for (let i = 0; i < nums.length; i++) {
+
+//       // guard clause for when result is found
+//       if (result.length != 0) break;
+
+//       // deduct num from target, to find the difference
+//       difference = target - nums[i]
+
+//       // check if leftover value exist in array
+//       if (
+//           nums.includes(difference) &&
+//           nums.indexOf(difference) != i &&
+//           target - nums[i] - difference === 0
+//           ) {
+
+//           // take the position of the two number into new array
+//           result.push(i);
+//           result.push(nums.indexOf(difference));
+//       }
+//   };
+
+//   return result;
